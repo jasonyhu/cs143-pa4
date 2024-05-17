07fa9b60640532f53ba0fa2aaf4d8155b0c859f7
hujason <hujason@stanford.edu>
2024-05-16 19:19:24 -0700

    compiles

--- pa3/cool-tree.handcode.h
+++ pa3/cool-tree.handcode.h
@@ -88,6 +88,7 @@ class ClassTable;
 
 #define formal_EXTRAS				  \
   void dump_with_types(ostream&,int); \
+ Symbol get_name() { return name; } \
   Symbol get_type_decl() { return type_decl; } \
   void traverse(ClassTable* classes, SymbolTable<Symbol, std::map<Symbol, Classes>>& methods, SymbolTable<Symbol, Class__class>& objects, Class_ errClass); \
 
