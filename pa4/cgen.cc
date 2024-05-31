2e3784a1c3a88fc44b1d470d38e01c641f6e07fb
hujason <hujason@stanford.edu>
2024-05-30 22:41:02 -0700

    move tests

--- pa4/cgen.cc
+++ pa4/cgen.cc
@@ -1241,7 +1241,9 @@ void dispatch_class::code(ostream &s, Environment env) {
   if (expr->get_type() != SELF_TYPE) {
     cur_class = expr->get_type();
   }
+  // seg faults here
   CgenNodeP cur_class_node; 
+  // codegen_classtable->get_class_node(cur_class);
   for (CgenNodeP nd : env.nds) {
       if (cur_class == nd->get_name()) {
         cur_class_node = nd;
@@ -1249,8 +1251,8 @@ void dispatch_class::code(ostream &s, Environment env) {
       }
   }
   emit_load(T1, 2, ACC, s);
-  // int id = cur_class_node->get_method_ids().at(name);
-  emit_load(T1, 3, T1, s); // bad magic number
+  int id = cur_class_node->get_method_ids().at(name);
+  emit_load(T1, id, T1, s);
   emit_jalr(T1, s);
 }
 
